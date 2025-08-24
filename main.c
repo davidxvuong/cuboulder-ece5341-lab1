88ada1e724ce71ae6025e0c46f81dca844100735
David Vuong <david.vuong@uwaterloo.ca>
2025-08-24 00:58:32 -0700

    Quick test to turn on/off MOSFET to spin DC motor

--- main.c
+++ main.c
@@ -1,26 +1,39 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
+/*
+ * ECEA 5341 Lab1 - DC Motors Lab
+ * Author: David Vuong
+ */
 #include "project.h"
+
+void Init_ADC()
+{
+}
+
+void Init_LCD()
+{
+    LCD_Start();
+    LCD_Position(0,0);
+    LCD_PrintString("DC Motors Lab");
+    CyDelay(1000);
+}
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    Init_LCD();
+    
     for(;;)
     {
-        /* Place your application code here. */
+        LCD_IsReady();
+        LCD_Position(1,0);
+        LCD_PrintString("GPIO ON");
+        P12_6_Write(1);
+        CyDelay(5000);
+        
+        LCD_IsReady();
+        LCD_Position(1,0);
+        LCD_PrintString("GPIO OFF");
+        P12_6_Write(0);
+        CyDelay(5000);
     }
 }
 
-/* [] END OF FILE */
